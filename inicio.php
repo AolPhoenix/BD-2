853161728b1ccd277b020d9bff42102b6894fa1b
AolPhoenix <matias.silvaal@sansano.usm.cl>
2019-07-18 03:02:59 -0400

    Modificacion #6
    Deberia soportar cualquier tipo de resolucion

--- inicio.php
+++ inicio.php
@@ -9,14 +9,19 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
 
 <link href="estilos.css" rel="stylesheet">
-<div id="cf4a" class="shadow" style="position:fixed; left:685px; top:196px; z-index:1;">
+<div class="fadein" style="display: inline-block; position:relative; left:700px; bottom:283; z-index:1;">
   <img src="imagenes/uno.jpg" style="width: 247px; height:440px;">
   <img src="imagenes/dos.jpg" style="width: 247px; height:440px;">
   <img src="imagenes/tres.jpg" style="width: 247px; height:440px;">
   <img src="imagenes/cuatro.jpg" style="width: 247px; height:440px;">
 </div>
-<IMG SRC = imagenes/background.png class="Telefono">
-<div class="CajaInicial">
+<IMG SRC = imagenes/background.png class="Telefono" style="position:relative;bottom:110px; left:10px;">
+<div class="CajaInicial" style="top:50px;">
+  <span class="Condiciones">
+    Al registrarte, aceptas nuestras<br>
+     <a class="minia" target="_blank" href="https://help.instagram.com/581066165581870">Condiciones</a>
+     , la <a class="minia" target="_blank" href="https://help.instagram.com/519522125107875">Política de datos</a> y la <a class="minia" target="_blank" href="/legal/cookies/">Política de cookies</a>.
+  </span>
   <span class=TextoRegistrar>Regístrate para ver fotos y videos<br> de tus amigos.</texto></span>
   <IMG SRC = imagenes/facebook.png class="FacebookFoto">
   <IMG SRC = imagenes/presiogram.png class="presiogram">
@@ -31,15 +36,10 @@
   </form>
 </div>
 
-<div class="CajaDos">
+<div class="CajaDos" style="top:160;">
   <span class="cuenta">¿Tienes una cuenta? <a class="minia" href="login.php"><span class="cuentalink">Inicia sesión</span></a></span>
 </div>
-<span class="Condiciones">
-  Al registrarte, aceptas nuestras<br>
-   <a class="minia" target="_blank" href="https://help.instagram.com/581066165581870">Condiciones</a>
-   , la <a class="minia" target="_blank" href="https://help.instagram.com/519522125107875">Política de datos</a>
-    y la<br> <a class="minia" target="_blank" href="/legal/cookies/">Política de cookies</a>.
-</span>
+
 
 </body>
 </html>
